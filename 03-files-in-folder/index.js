27b27e986f0503ae45148eee19599c0f25a37c31
nos64 <manosov1984@gmail.com>
2022-05-20 15:55:46 +0400

    feat: start 04-task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -10,7 +10,7 @@ fs.readdir(
     files.forEach(item => {
       let file = path.resolve(folder, item.name);
       if (!item.isDirectory()) {
-        fs.stat(file, function (err, stats) {
+        fs.stat(file, (err, stats) => {
           if (err) console.log(err);
           console.log(`${path.parse(file).name} - ${path.parse(file).ext.slice(1)} - ${(stats.size / 1024)}kB`);
         });
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,92 @@
+const fs = require('fs');
+const path = require('path');
+
+const filesCopy = path.resolve(__dirname, 'files-copy');
+const files = path.join(__dirname, 'files');
+
+const deleteFile = (folder) => {
+  fs.readdir(folder,
+    { withFileTypes: true },
+    (err, files) => {
+    if(err) throw err;
+    files.forEach(item =>{
+      let file = path.resolve(folder, item.name);
+      fs.stat(file, (errStat, stats) => {
+        if(errStat) throw errStat;
+        if(stats.isDirectory()){
+          fs.rmdir((file), (err) => {
+            if (!err) fs.rmdir((file), () => {console.log('Удален файл ' + file);})
+            else {
+              deleteFile(file)
+              console.log('file: ', file);
+              
+            }
+          });
+       }else if (stats.isFile()) {
+        fs.unlink((folder, file), () => {});
+        } 
+        // else {
+        //   fs.rmdir((filesCopy, file), () => {})
+        // };
+      })
+    })
+  })
+}
+
+deleteFile(filesCopy)
+
+// const mkDir = () => {
+//   fs.stat(path.join(__dirname, 'files-copy'), (err, stats) => {
+//     if (err) { 
+//       fs.mkdir(path.join(__dirname, 'files-copy'), () => {});
+//     } else if (stats.isDirectory()) {
+//       fs.unlink(path.join(__dirname, 'files-copy'), () => {});
+//         // process.exit();
+//     } else if (stats.isFile()) {
+//       fs.unlink(path.join(__dirname, 'files-copy'), () => {});
+//       mkDir();
+//     } 
+//   });
+// };
+// mkDir();
+
+// fs.stat(path.join(__dirname, 'files-copy'), (err, stats) => {
+//   if (err) { 
+//     fs.mkdir(path.join(__dirname, 'files-copy'), () => {});
+//   } else {
+//     // fs.rmdir(path.join(__dirname, 'files-copy'), err => {});
+//     // fs.mkdir(path.join(__dirname, 'files-copy'), () => {});
+//     fs.readdir(path.join(__dirname, 'files-copy'), (err, files) => {
+//       if(err) throw err;
+//       files.forEach(item => {
+//         fs.stat(path.join(__dirname, 'files-copy'), (err, item) => {
+//           if(err) throw err;
+//           if(stats.isDerictory()){
+//             console.log('Папка: ' + item.name);
+//             listObjects(path + '/' + item.name);
+//           }else{
+//             console.log('Файл: ' + item.name);
+//          }
+//         })
+//       })
+//     })
+
+    
+//   }
+// })
+
+
+// fs.readdir(
+//   path.join(__dirname, 'files'),
+//   { withFileTypes: true },
+//   (err, files) => {
+//     if (err) console.log(err);
+//     files.forEach(item => {
+//       fs.copyFile(path.join(__dirname, 'files', item.name), path.join(__dirname, 'files-copy', item.name), (err) => {
+//         if (err) {
+//           console.log("Error Found:", err);
+//         }
+//       });
+//     });
+//   });
 
