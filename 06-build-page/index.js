93605efe4768970d9a74a3f4d444bc1fe7c54c97
Mikhail Nosov <manosov1984@gmail.com>
2022-05-22 22:38:57 +0400

    feat: 06-task add create index.html & style.css

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 const path = require('path');
+const { stdin, stdout } = require('process');
 
 const projectDist = path.join(__dirname, 'project-dist');
 const assets = path.join(__dirname, 'assets');
 const initialStyles = path.join(__dirname, 'styles');
+const components = path.join(__dirname, 'components');
 
 function mkDir() {
   fs.mkdir((projectDist), () => { });
@@ -30,6 +32,45 @@ function mergeStyle () {
       });
     });
 }
+
+
+//Читаем файл template
+fs.readFile(path.join(__dirname, 'template.html'), 'utf8', (err, data) => {
+  if (err) console.log(err);
+  let idexData = '';
+  idexData += data;
+
+  //Читаем папку с компонентами
+  fs.readdir(components,
+    { withFileTypes: true },
+    (err, files) => {
+      if (err) console.log(err);
+
+      //Перебираем компоненты
+      files.forEach(item => {
+        let file = path.join(components, item.name);
+        fs.stat(file, (err, stats) => {
+          if (err) console.log(err);
+
+          //Для каждого компонента с расширением html
+          if(stats.isFile() && path.parse(file).ext.slice(1) === 'html') {
+
+            //Читаем компонент
+            fs.readFile(file, 'utf8', (err, data) => {
+              if (err) console.log(err);
+              //Заменяем {{компонент}} на содержимое файла компонента
+              idexData = idexData.replace(`{{${path.parse(file).name}}}`, data);
+
+              //Записываем в файл index.html
+              fs.writeFile(path.join(projectDist, 'index.html'), idexData, err => {
+                if (err) console.log(err);
+              });
+            });
+          }
+        });
+      });
+    });
+});
 
 mkDir();
 mergeStyle();
